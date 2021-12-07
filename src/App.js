324bdad153a95285f63a72d3a16d20305a727ea6
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-12-07 18:26:47 +0900

    style:Updated animation & pseudo selector

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Father = styled.div`
   display: flex;
+`;
+
+const Emoji = styled.span`
+  color: ghostwhite;
+`;
+
+const rotateAnimation = keyframes`
+0%{
+  transform:rotate(0deg);
+  border-radius:0%;
+}
+50%{
+  border-radius:50%;
+}
+100%{
+  transform:rotate(360deg);
+  border-radius:0%;
+}
+
 `;
 
 const Box = styled.div`
   background-color: ${(props) => props.bgColor};
   height: 100px;
   width: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotateAnimation} ease-in-out 1s infinite;
+  ${Emoji} {
+    &:hover {
+      font-size: 50px;
+    }
+    &:active {
+      opacity: 10%;
+    }
+  }
 `;
 
 const Circle = styled(Box)`
@@ -28,10 +59,13 @@ function App() {
     <>
       <Father as="header">
         <Box bgColor="tomato">
-          <Text>Hello</Text>
+          <Emoji as="p">(●'◡'●)</Emoji>
         </Box>
-        <Circle bgColor="teal" />
+        <Circle bgColor="teal">
+          <Text>Hello</Text>
+        </Circle>
       </Father>
+      <Emoji>(❁´◡`❁)</Emoji>
       <Input />
       <Input />
       <Input />
