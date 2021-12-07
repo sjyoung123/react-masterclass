19d74e43e86610fe0c2693334e77775db0f1dc96
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-12-07 21:07:55 +0900

    Updated props

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
-import styled from "styled-components";
-
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.backgroundColor};
-`;
+import Circle from "./Circle";
 
 function App() {
   return (
-    <Wrapper>
-      <Title>Hello</Title>
-    </Wrapper>
+    <>
+      <div>
+        <Circle borderColor="blue" bgColor="tomato" />
+        <Circle text="hi" bgColor="teal" />
+      </div>
+    </>
   );
 }
 
---
+++ src/Circle.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+
+interface ContainerProps {
+  bgColor: string;
+  borderColor: string;
+}
+
+const Container = styled.div<ContainerProps>`
+  width: 200px;
+  height: 200px;
+  background-color: ${(props) => props.bgColor};
+  border-radius: 50%;
+  border: 2px solid ${(props) => props.borderColor};
+`;
+
+interface CircleProps {
+  bgColor: string;
+  borderColor?: string;
+  text?: string;
+}
+
+const Circle = ({
+  bgColor,
+  borderColor,
+  text = "default text",
+}: CircleProps) => {
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>
+  );
+};
+
+export default Circle;
